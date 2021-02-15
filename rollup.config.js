8e5bc95a389a0fc61d80270e13b277aec67f0aac
BrandonVM26 <Brandon.Valverde@davia.com>
2021-02-14 22:00:31 -0600

    Test

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 import babel from '@rollup/plugin-babel';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import del from 'rollup-plugin-delete';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import pkg from './package.json';
-import postcss from "rollup-plugin-postcss";
-import postcssUrl from 'postcss-url';
+import resolve from "@rollup/plugin-node-resolve";
+import scssSmartAsset from 'rollup-plugin-scss-smart-asset';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -16,11 +15,11 @@ export default {
     ],
     plugins: [
         peerDepsExternal(),
-        postcss({
-            inject: true,
-            plugins: [
-                postcssUrl({ url: 'inline' })                
-            ]            
+        scssSmartAsset({
+            insert: true,
+            postcssUrlConfig: {
+                url: 'inline'
+            }
         }),
         babel({
             exclude: 'node_modules/**'
